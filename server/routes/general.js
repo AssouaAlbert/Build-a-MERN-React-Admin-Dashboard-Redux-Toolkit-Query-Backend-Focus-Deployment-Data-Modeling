c998b9a516873c2a4991bce4548e8602e4879d85
Albert Assoua <assouaalbert@gmail.com>
2023-04-21 20:45:38 -0400

    Creat Dashboard endpoint

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,8 +1,10 @@
 import express from "express";
 import getUser from "../controllers/general.js";
+import getDashboard from "../controllers/dashboard.js";
 
 const router = express.Router();
 
 router.get("/user/:id", getUser);
+router.get("/dashboard", getDashboard);
 
 export default router;
